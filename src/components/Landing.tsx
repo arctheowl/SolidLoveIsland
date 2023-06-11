30de05b4147863f510e04677ce38548707ab654c
lidevlin <lidevlin95@gmail.com>
2023-06-11 15:08:24 +0100

    improved landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -8,9 +8,11 @@ type Props = {
 
 const Landing = (props: Props) => {
   return (
-    <div class="bg-red-200 h-screen w-screen items-center flex flex-col justify-center">
-      <h1 class="text-2xl font-bold text-center"> Love Island Tracker</h1>
-      <div class=" ">
+    <div class="bg-[#fdf5df] h-screen w-screen items-center flex flex-col justify-center ">
+      <div class=" p-24 rounded-xl">
+        <h1 class="text-7xl font-extrabold text-center from-[#F42D69] bg-gradient-to-r to-[#FF8A00] text-transparent bg-clip-text pb-10">
+          Love Island Tracker
+        </h1>
         <select
           class="rounded-full border-2 p-3 bg-[#5ebec4] mx-auto block "
           onChange={(e) => props.setSelection(e.target.value)}
