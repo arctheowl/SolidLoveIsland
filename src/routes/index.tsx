f7aeeff8928190cd5483c0635d46a1ec7934f5f6
lidevlin <lidevlin95@gmail.com>
2023-06-12 17:09:11 +0100

    Used switch instead of show

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Show, createEffect, createSignal, For } from "solid-js";
+import { Show, createEffect, createSignal, For, Switch, Match } from "solid-js";
 import data from "/data/2023/ContestantList.json";
 import Bio from "~/components/Bio";
 import Partner from "~/components/Partner";
@@ -15,48 +15,55 @@ export default function Home() {
 
   return (
     <main class="text-gray-700 bg-[#fdf5df] h-screen flex flex-col justify-center align-middle items-center">
-      <Show when={selection() == "Select Contestant"}>
-        <Landing selection={selection} setSelection={setSelection} />
-      </Show>
-      <Show when={selection() != "Select Contestant"}>
-        <div class="flex flex-col items-center h-screen w-screen p-4 md:grid md:grid-cols-12 md:gap-0">
-          <div class="flex gap-5 md:col-span-12 md:mx-auto">
-            <Selection selection={selection} setSelection={setSelection} />
-          </div>
-          <Show
-            when={data.find(
-              (contestant: any) =>
-                contestant.name == selection() && contestant.dumped == true
-            )}
-          >
-            <div class="col-span-12">
-              <Dumped selection={selection} />
-            </div>
-          </Show>
-          <Show
-            when={data.find(
+      <Switch fallback={<div>404</div>}>
+        <Match when={selection() == "Select Contestant"}>
+          <Landing selection={selection} setSelection={setSelection} />
+        </Match>
+        <Match
+          when={
+            selection() != "Select Contestant" &&
+            data.find(
               (contestant: any) =>
                 contestant.name == selection() && contestant.dumped == false
-            )}
-          >
-            <>
-              <div class="md:col-span-5">
-                <Bio selection={selection} main={true} />
-              </div>
-              <Middle selection={selection} />
-              <div class="md:col-span-5">
-                <Partner selection={selection} setSelection={setSelection} />
-              </div>
-              <div class="md:col-span-5 pt-2">
-                <PastPartners selection={selection} />
-              </div>
-              <div class="md:col-span-5 pt-2 w-full">
-                <Facts selection={selection} />
-              </div>
-            </>
-          </Show>
-        </div>
-      </Show>
+            )
+          }
+        >
+          <div class="flex flex-col items-center h-screen w-screen p-4 md:grid md:grid-cols-12 md:gap-0">
+            <div class="flex gap-5 md:col-span-12 md:mx-auto">
+              <Selection selection={selection} setSelection={setSelection} />
+            </div>
+            <div class="md:col-span-5">
+              <Bio selection={selection} main={true} />
+            </div>
+            <Middle selection={selection} />
+            <div class="md:col-span-5">
+              <Partner selection={selection} setSelection={setSelection} />
+            </div>
+            <div class="md:col-span-5 pt-2">
+              <PastPartners selection={selection} />
+            </div>
+            <div class="md:col-span-5 pt-2 w-full">
+              <Facts selection={selection} />
+            </div>
+          </div>
+        </Match>
+        <Match
+          when={
+            selection() != "Select Contestant" &&
+            data.find(
+              (contestant: any) =>
+                contestant.name == selection() && contestant.dumped == true
+            )
+          }
+        >
+          <div class="flex flex-col items-center h-screen w-screen p-4">
+            <div class="flex gap-5 md:col-span-12 md:mx-auto">
+              <Selection selection={selection} setSelection={setSelection} />
+            </div>
+            <Dumped selection={selection} />
+          </div>
+        </Match>
+      </Switch>
     </main>
   );
 }
